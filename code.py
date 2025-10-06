71e690d900bd43311db87e7350e2d93e890cf61a
danicabu1402-sketch <danicabu1402@gmail.com>
2025-10-05 23:33:42 -0500

    Punto 4 (suplots)

--- code.py
+++ code.py
@@ -14,3 +14,14 @@ print(matriz_T)
 print(matriz_F)
 print(matriz_C)
 
+fig = plt.figure(figsize=(14, 10))
+
+ax1 = plt.subplot2grid((3, 3), (0, 0), colspan=2)
+ax2 = plt.subplot2grid((3, 3), (0, 2))
+ax3 = plt.subplot2grid((3, 3), (1, 0))
+ax4 = plt.subplot2grid((3, 3), (1, 1))
+ax5 = plt.subplot2grid((3, 3), (1, 2))
+ax6 = plt.subplot2grid((3, 3), (2, 0), colspan=3)
+
+
+
