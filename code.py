97d36a699d117e9b2ce1f1ca7d03ed38abccd43c
danicabu1402-sketch <danicabu1402@gmail.com>
2025-10-05 23:31:46 -0500

    Punto 3 (F,C)

--- code.py
+++ code.py
@@ -6,3 +6,11 @@ print(vector)
 matriz = vector.reshape((120, 6))
 print(matriz)
 
+
+matriz_T = matriz.T
+matriz_F = np.copy(np.reshape(matriz_T, matriz_T.shape, order='F'))
+matriz_C = np.copy(np.reshape(matriz_T, matriz_T.shape, order='C'))
+print(matriz_T)
+print(matriz_F)
+print(matriz_C)
+
